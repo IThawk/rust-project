356944799ed8e2f0e6542eaa7356598f16b1d528
lianwei <lianwei1994@163.com>
2019-06-30 15:57:28 +0800

    helloworld

--- rust-demo/hello_world/src/main.rs
+++ rust-demo/hello_world/src/main.rs
@@ -1,4 +1,29 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, kkkkworld!");
-    println!("Hello, kkkkworld!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
 }
