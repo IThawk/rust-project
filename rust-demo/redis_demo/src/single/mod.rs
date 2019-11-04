7811928838ae8b887355431690d9ec4103d00670
IThawk <lianwei@163.com>
2019-11-05 00:03:54 +0800

    redis

---
+++ rust-demo/redis_demo/src/single/mod.rs
@@ -0,0 +1,2 @@
+mod redis_single_demo;
+pub use self::redis_single_demo::{single_redis_main};
---
+++ rust-demo/redis_demo/src/single/redis_single_demo.rs
@@ -0,0 +1,29 @@
+use redis::{Commands, Connection, RedisResult};
+
+pub fn single_redis_main() {
+    if let Ok(mut conn) = get_connection("redis://192.168.101.13:16379") {
+        set_string_value("test1", "test1", &mut conn);
+    };
+    println!("ddddd");
+}
+
+
+fn get_connection(path: &str) -> RedisResult<(Connection)> {
+    let client = redis::Client::open(path)?;
+    let mut con = client.get_connection()?;
+
+    /* do something here */
+
+    Ok((con))
+}
+
+fn set_string_value(key: &str, value: &str, con: &mut Connection) -> Result<(), String> {
+    match con.set(key, value) {
+        Ok(()) => Ok(()),
+        Err(e) => {
+            let a = format!("set key:{},value:{},error:{:?}", key, value, e);
+            Err(a)
+        }
+    }
+}
+
