b5d94c4010953ef4aa91ad09c26f879c9af72a08
IThawk <lianwei@163.com>
2019-12-10 23:47:21 +0800

    添加ｌｉｎｕｘ代码

--- rust-demo/tokio_1_demo/src/example/proxy.rs
+++ rust-demo/tokio_1_demo/src/example/proxy.rs
@@ -51,9 +51,54 @@ fn main() -> Result<(), Box<std::error::Error>> {
             println!("Proxying to: {}", server_addr);
 
             let a = UnixListener::bind("/root.socket")?;
+            let done = a
+                .incoming()
+                .map_err(|e| println!("error accepting socket; error = {:?}", e))
+                .for_each(move |client| {
+                    let server = UnixStream::connect("/root.socket");
+                    let amounts = server.and_then(move |server| {
+                        // Create separate read/write handles for the TCP clients that we're
+                        // proxying data between. Note that typically you'd use
+                        // `AsyncRead::split` for this operation, but we want our writer
+                        // handles to have a custom implementation of `shutdown` which
+                        // actually calls `TcpStream::shutdown` to ensure that EOF is
+                        // transmitted properly across the proxied connection.
+                        //
+                        // As a result, we wrap up our client/server manually in arcs and
+                        // use the impls below on our custom `MyTcpStream` type.
+                        let client_reader = MyUdsTcpStream(Arc::new(Mutex::new(client)));
+                        let client_writer = client_reader.clone();
+                        let server_reader = MyUdsTcpStream(Arc::new(Mutex::new(server)));
+                        let server_writer = server_reader.clone();
 
+                        // Copy the data (in parallel) between the client and the server.
+                        // After the copy is done we indicate to the remote side that we've
+                        // finished by shutting down the connection.
+                        let client_to_server = copy(client_reader, server_writer)
+                            .and_then(|(n, _, server_writer)| shutdown(server_writer).map(move |_| n));
 
-            let b = a.incoming().map_err(|e| println!("error accepting socket; error = {:?}", e)).for_each(|c| {});
+                        let server_to_client = copy(server_reader, client_writer)
+                            .and_then(|(n, _, client_writer)| shutdown(client_writer).map(move |_| n));
+
+                        client_to_server.join(server_to_client)
+                    });
+
+                    let msg = amounts
+                        .map(move |(from_client, from_server)| {
+                            println!(
+                                "client wrote {} bytes and received {} bytes",
+                                from_client, from_server
+                            );
+                        })
+                        .map_err(|e| {
+                            // Don't panic. Maybe the client just disconnected too soon.
+                            println!("error: {}", e);
+                        });
+
+                    tokio::spawn(msg);
+
+                    Ok(())
+                });
         }
     #[cfg(target_os = "windows")]
         {
@@ -146,3 +191,34 @@ impl AsyncWrite for MyTcpStream {
         Ok(().into())
     }
 }
+
+
+#[derive(Clone)]
+struct MyUdsTcpStream(Arc<Mutex<UnixStream>>);
+
+impl Read for MyUdsTcpStream {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.lock().unwrap().read(buf)
+    }
+}
+
+impl Write for MyUdsTcpStream {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.lock().unwrap().write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl AsyncRead for MyUdsTcpStream {}
+
+impl AsyncWrite for MyUdsTcpStream {
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        self.0.lock().unwrap().shutdown(Shutdown::Write)?;
+        Ok(().into())
+    }
+}
+
+
