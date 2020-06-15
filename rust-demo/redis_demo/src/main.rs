ec8e48189cf4c12ddae1c958d924ca429ee3917e
IThawk <lianwei1994@163.com>
2020-06-15 22:52:05 +0800

    ddd

--- rust-demo/redis_demo/src/main.rs
+++ rust-demo/redis_demo/src/main.rs
@@ -1,17 +1,29 @@
 extern crate redis;
+
 mod pool;
 mod single;
 
 use pool::*;
 use single::*;
+use std::time::{Duration, Instant};
+use std::thread;
 
 fn main() {
     //获取环境变量 Redis
     let a = std::env::var("redis").unwrap_or("hello".to_string());
     println!("{}", a);
-    single_redis_main();
+    //    single_redis_main();
     println!("Hello, world!");
-    //    pool_redis_main();
+    loop {
+        let a = Duration::from_secs(5);
+        thread::sleep(a);
+        my_redis_pool_demo::my_pool_redis_main();
+    }
+
 
     //    single_pub_sub_main();
 }
+
+
+
+
